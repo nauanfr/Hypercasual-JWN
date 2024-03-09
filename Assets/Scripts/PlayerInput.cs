5c986b461a78db53b8ac6e1e51e0d301928ae6da
nauanfr <nauan@me.com>
2024-03-08 21:26:55 -0300

    Clique na tela mudando de cor.

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
+    private MeshRenderer meshRenderer;
+    private Color[] colors = { Color.red, Color.green, Color.blue };
+    private int currentIndex = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        meshRenderer.material.color = colors[currentIndex];
     }
 
     // Update is called once per frame
@@ -15,6 +21,8 @@ public class PlayerInput : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            currentIndex = (currentIndex + 1) % colors.Length;
+            meshRenderer.material.color = colors[currentIndex];
             Debug.Log("Clicou");
         }
     }
